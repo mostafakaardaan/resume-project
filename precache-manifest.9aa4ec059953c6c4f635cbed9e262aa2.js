4f9b85bff7183b1c16ee98d706626c4dc6b6f1ce
Mostafa Kaardaan <mostafa.kaardaan@yahoo.com>
2019-04-02 19:53:49 +0430

    Updates

---
+++ precache-manifest.9aa4ec059953c6c4f635cbed9e262aa2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d36ca01aa83d8451351c",
+    "url": "/resume-project/static/css/main.cd73f46d.chunk.css"
+  },
+  {
+    "revision": "d36ca01aa83d8451351c",
+    "url": "/resume-project/static/js/main.29492d07.chunk.js"
+  },
+  {
+    "revision": "3d01deb95f5dbba8dd25",
+    "url": "/resume-project/static/js/runtime~main.604347a4.js"
+  },
+  {
+    "revision": "eb52a1e46d57d4dd24fe",
+    "url": "/resume-project/static/js/2.6b7d04e3.chunk.js"
+  },
+  {
+    "revision": "6ca15d62bf9cbcad5df73dbb1167169e",
+    "url": "/resume-project/index.html"
+  }
+];
